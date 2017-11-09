2918e42ab1b999b1b21523c1f1a024591d299e5d
Annie Nguyen <aknguyen@us.ibm.com>
2017-11-09 15:37:19 -0600

    configure via injection

--- js/interactive-guides/microprofile-config/microprofile-config-callback.js
+++ js/interactive-guides/microprofile-config/microprofile-config-callback.js
@@ -79,6 +79,8 @@ var microprofileConfigCallBack = (function() {
         // correct annotation/method
         if (stepName === "ConfigureAsEnvVar") {
             __addPropToServerEnv(stepName);
+        } else if (stepName === "ConfigureViaInject") {
+            __addInjectConfigToEditor(stepName);
         }
         // hide the error box
         __closeEditorErrorBox(stepName);
@@ -158,15 +160,7 @@ var microprofileConfigCallBack = (function() {
         }
     };
 
-    var __saveServerEnvButton = function(event) {
-        if (event.type === "click" ||
-           (event.type === "keypress" && (event.which === 13 || event.which === 32))) {
-            // Click or 'Enter' or 'Space' key event...
-            contentManager.saveEditor(stepContent.getCurrentStepName());
-        }
-    };
-
-    var __serverEnvRefreshBrowserButton = function(event) {
+    var __refreshBrowserButton = function(event) {
         if (event.type === "click" ||
            (event.type === "keypress" && (event.which === 13 || event.which === 32))) {
            // Click or 'Enter' or 'Space' key event...
@@ -174,12 +168,167 @@ var microprofileConfigCallBack = (function() {
         }
     };
 
+    var __saveButton = function(event) {
+        if (event.type === "click" ||
+           (event.type === "keypress" && (event.which === 13 || event.which === 32))) {
+            // Click or 'Enter' or 'Space' key event...
+            contentManager.saveEditor(stepContent.getCurrentStepName());
+        }
+    };
+   
+    //var injectConfigDefault = "download-url=ftp://music.com/us/download";
+
+    var __getInjectionConfigContent = function(content) {
+        var editorContents = {};
+        try {
+            // match
+            // private Config config;
+            //   <space or newline here>
+            // @Inject @ConfigProperty(name=\"download-url\", defaultValue=\"ftp://music.com/us/download\")
+            // private String downloadUrl;
+            var contentToMatch = "[\\s\\S]*private Config config;\\s*@Inject\\s*@ConfigProperty\\s*\(([\\s\\S]*)\)\\s*private String downloadUrl;";
+            var regExpToMatch = new RegExp(contentToMatch, "g");
+            var groups = regExpToMatch.exec(content);
+            
+            var params = groups[1];
+            params = params.replace('\n','');
+            params = params.replace(/\s/g, ''); // Remove whitespace
+            if (params.trim() !== "") {
+                params = params.split(',');
+            } else {
+                params = [];
+            }
+            editorContents.annotationParams = params;           
+        }
+        catch (e) {
+
+        }
+        return editorContents;
+    };
+
+    /*
+      Match the parameters. Returns
+        0 for no match
+        1 for exact match
+        2 for extra parameters
+    */
+    var __isParamInAnnotation = function(annotationParams, paramsToCheck) {
+        var params = [];
+        var allMatch = 1;  // assume matching to begin with
+
+        // for each parameter, break it down to name and value so as to make it easier to compare
+        $(annotationParams).each(function(index, element){
+            if (element.indexOf("=") !== -1) {
+                params[index] = {};
+                params[index].value = element.trim().substring(element.indexOf('=') + 1);
+                params[index].name = element.trim().substring(0, element.indexOf('='));
+            }
+        });
+        // now compare with the passed in expected params
+        $(paramsToCheck).each(function(index, element){
+            if (element.indexOf("=") !== -1) {
+                var value = element.trim().substring(element.indexOf('=') + 1);
+                var name = element.trim().substring(0, element.indexOf('='));
+                var eachMatch = false;
+                $(params).each(function(paramsIndex, annotationInEditor) {
+                    if (annotationInEditor.name === name && annotationInEditor.value === value) {
+                        eachMatch = true;
+                        return false;  // break out of each loop
+                    }
+                });
+                if (eachMatch === false) {
+                    allMatch = 0;
+                    return false; // break out of each loop
+                }
+            }
+        });
+
+        if (allMatch === 1 && annotationParams.length > paramsToCheck.length) {
+            allMatch = 2; // extra parameters
+        }
+        console.log("allMatch ", allMatch);
+        return allMatch;
+    };
+
+    var __checkInjectionEditorContent = function(content) {
+        var annotationIsThere = true;
+        var editorContentBreakdown = __getInjectionConfigContent(content);
+        if (editorContentBreakdown.hasOwnProperty("annotationParams")) {
+            var paramsToCheck = [];
+            paramsToCheck[0] = "name=\"download-url\"";
+            paramsToCheck[1] = "defaultValue=\"ftp://music.com/us/download\"";
+            var isParamInAnnotation = __isParamInAnnotation(editorContentBreakdown.annotationParams, paramsToCheck);
+            if (isParamInAnnotation !== 1) {
+                annotationIsThere = false;
+            }
+        } else {
+            annotationIsThere = false;
+        }
+        //console.log("")
+        return annotationIsThere;
+    };
+
+    var __listenToEditorForInjectConfig = function(editor) {
+        var __showWebBrowser = function() {
+            var stepName = editor.getStepName();
+            var content = contentManager.getEditorContents(stepName);
+            if (__checkInjectionEditorContent(content)) {
+                __closeEditorErrorBox(stepName);
+                contentManager.showBrowser(stepName, 0);
+                contentManager.addRightSlideClassToBrowser(stepName, 0);
+                contentManager.markCurrentInstructionComplete(stepName);
+                //contentManager.updateWithNewInstructionNoMarkComplete(stepName);
+            } else {
+                // display error and provide link to fix it
+                __createErrorLinkForCallBack(stepName, true);
+            }
+        };
+        editor.addSaveListener(__showWebBrowser);
+    };
+
+    var __addConfigInjectButton = function(event) {
+        if (event.type === "click" ||
+        (event.type === "keypress" && (event.which === 13 || event.which === 32))) {
+            // Click or 'Enter' or 'Space' key event...
+            __addInjectConfigToEditor();
+        }        
+    };
+
+    var __addInjectConfigToEditor = function(stepName) {
+        var injectConfig = "    @Inject @ConfigProperty(name=\"download-url\", defaultValue=\"ftp://music.com/us/download\")";
+        if (!stepName) {   
+           stepName = stepContent.getCurrentStepName();
+        }
+        // reset content every time property is added through the button so as to clear out any
+        // manual editing
+        __closeEditorErrorBox(stepName);
+        contentManager.resetEditorContents(stepName);
+        var content = contentManager.getEditorContents(stepName);
+
+        contentManager.insertEditorContents(stepName, 9, injectConfig);
+        var readOnlyLines = [];
+        readOnlyLines.push({from: 1, to: 7}, {from: 10, to: 11});
+        contentManager.markEditorReadOnlyLines(stepName, readOnlyLines);       
+    };
+
+    var __listenToBrowserForDefaultConfig = function(webBrowser) {
+        var setBrowserContent = function(currentURL) {
+            webBrowser.setBrowserContent("/guides/iguide-microprofile-config/html/interactive-guides/microprofile-config/download-from-injection.html");
+            contentManager.markCurrentInstructionComplete(webBrowser.getStepName());
+        }
+        
+        webBrowser.addUpdatedURLListener(setBrowserContent);
+    };
+
     return {
         listenToEditorForServerEnv: __listenToEditorForServerEnv,
         listenToBrowserForServerEnvConfig: __listenToBrowserForServerEnvConfig,
         addPropToServerEnvButton: __addPropToServerEnvButton,
-        saveServerEnvButton: __saveServerEnvButton,
-        serverEnvRefreshBrowserButton: __serverEnvRefreshBrowserButton
+        refreshBrowserButton: __refreshBrowserButton,
+        saveButton: __saveButton,
+        listenToEditorForInjectConfig: __listenToEditorForInjectConfig,
+        addConfigInjectButton: __addConfigInjectButton,
+        listenToBrowserForDefaultConfig:  __listenToBrowserForDefaultConfig    
     };
 
 })();
