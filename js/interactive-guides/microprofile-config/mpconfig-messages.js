9193269496ea471a45f41ee0f784257f84896350
steven1046 <stevenzvonek1992@gmail.com>
2017-12-13 13:41:57 -0600

    Playground clarifications and improvements

--- js/interactive-guides/microprofile-config/mpconfig-messages.js
+++ js/interactive-guides/microprofile-config/mpconfig-messages.js
@@ -1,4 +1,4 @@
 var mpconfigMessages = {
-    'injectionRequired': 'The property <b>{0}</b> needs to be set with @Inject in Java file',
+    'injectionRequired': 'The property <b>{0}</b> needs to be set with @Inject in CarTypes.java',
     'valueRequired': 'DeploymentException: <b>{0}</b> must be assigned a value.'
 };
